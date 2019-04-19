4f25387d80a8938f949c2c4cf21286d8e0b3d86e
Sinkerine <git@15cm.net>
2019-04-19 18:23:17 -0400

    Fix mysql client command

--- run.sh
+++ run.sh
@@ -9,9 +9,20 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-qiandao}
 export JAWSDB_MARIA_URL="http://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}"
 
 # Create tables if not exists. Won't overwrite tables if exists
-mysql -h ${MYSQL_HOST} -P ${MYSQL_PORT} \
-      -u ${MYSQL_USER} -p ${MYSQL_PASSWORD} \
-      -e "source /qiandao/qiandao.sql" \
-      ${MYSQL_DATABASE}
+
+mysql_exec() {
+  command=$1
+  mysql -s \
+        -h ${MYSQL_HOST} -P ${MYSQL_PORT} \
+        -u ${MYSQL_USER} --password=${MYSQL_PASSWORD} \
+        -e "${command}" \
+        ${MYSQL_DATABASE}
+}
+
+until mysql_exec ';'; do
+  sleep 3
+done
+
+mysql_exec 'source /qiandao/qiandao.sql'
 
 python /qiandao/run.py
